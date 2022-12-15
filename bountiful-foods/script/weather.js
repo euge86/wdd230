be4dfe75b8e77b3cbace4217077a1d7071bafcda
euge86 <eugeespejo@gmail.com>
2022-12-15 02:56:10 -0300

    done

--- bountiful-foods/script/weather.js
+++ bountiful-foods/script/weather.js
@@ -40,13 +40,13 @@ async function apiFetch() {
     .then((jsObject) => {
         
         let weekday = new Array(7);
-        weekday[0] = "Sun";
-        weekday[1] = "Mon";
-        weekday[2] = "Tue";
-        weekday[3] = "Wed";
-        weekday[4] = "Thu";
-        weekday[5] = "Fri";
-        weekday[6] = "Sat";
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
 
         let data = jsObject.list.filter((element)=>element.dt_txt.includes('00:00:00'));
         console.log(data);
